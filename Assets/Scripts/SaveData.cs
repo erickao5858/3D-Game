ff11269781d0e181eb5ca4b47a7cc999d8f31dda
ERIC KAO <kaoch@deakin.edu.au>
2019-12-05 16:34:16 +1100

    Implement save system UI

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    public List<Attribute> attributes = new List<Attribute>();
+    public Vector3 playerPosition;
+    public Quaternion playerRotation;
+}
---
+++ Assets/UI/Scripts/ButtonBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public GameObject character;
+    private SaveData saveData;
+    public void SaveGame()
+    {
+        List<Attribute> attributes = new List<Attribute>();
+        foreach (Attribute attribute in character.GetComponents<Attribute>())
+        {
+            attributes.Add(attribute);
+        }
+        saveData = gameObject.AddComponent<SaveData>();
+        saveData.attributes = attributes;
+        saveData.playerPosition = character.transform.position;
+        saveData.playerRotation = character.transform.rotation;
+    }
+    public void LoadGame()
+    {
+        //character.transform.position = aaa.playerPosition;
+        //character.transform.rotation = aaa.playerRotation;
+    }
+}
