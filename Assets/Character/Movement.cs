15efcbbcd33357d4818003b567d20a85b7647731
MINGYI LI <limingy@deakin.edu.au>
2019-12-10 10:25:48 +1100

    Implement character movement

---
+++ Assets/Character/Movement.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Animator anim;
+    public Rigidbody rbody;
+    private float inputH;
+    private float inputV;
+    private bool run;
+    public float turnspeed = 1;
+    [SerializeField] LayerMask whatIsGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody>();
+        run = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            anim.Play("WAIT01", -1, 0f);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            anim.Play("WAIT02", -1, 0f);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            anim.Play("WAIT03", -1, 0f);
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            anim.Play("WAIT04", -1, 0f);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            int random = Random.Range(0, 2);
+            if (random == 0)
+            {
+                anim.Play("DAMAGED00", -1, 0f);
+            }
+            else
+            {
+                anim.Play("DAMAGED01", -1, 0f);
+            }
+
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            run = true;
+        }
+        else
+        {
+            run = false;
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("jump", true);
+        }
+        else
+        {
+            anim.SetBool("jump", false);
+        }
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+        anim.SetBool("run", run);
+
+        float moveX = inputH * 20f * Time.deltaTime;
+        float moveZ = inputV * 50f * Time.deltaTime;
+        if(run)
+        {
+            moveX *= 3f;
+            moveZ *= 3f;
+        }
+        rbody.velocity = new Vector3(moveX, 0f, moveZ);
+    }
+    void Rotating(float hor, float ver)
+    {
+        //获取方向
+        Vector3 dir = new Vector3(hor, 0, ver);
+        //将方向转换为四元数
+        Quaternion quaDir = Quaternion.LookRotation(dir, Vector3.up);
+        //缓慢转动到目标点
+        transform.rotation = Quaternion.Lerp(transform.rotation, quaDir, Time.fixedDeltaTime * turnspeed);
+
+
+
+
+    }
+
+
+
+    void FixedUpdate()
+    {
+
+
+        
+        if (inputH != 0 || inputV != 0)
+        {
+            //转身
+            Rotating(inputH, inputV);
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
